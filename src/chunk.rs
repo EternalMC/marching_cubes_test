9a69100a459c4bbb58196f7955fc56a00b98a6f0
EternalMC <55027815+EternalMC@users.noreply.github.com>
2020-01-31 09:35:40 -0600

    Update chunk.rs

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,9 +1,9 @@
 use isosurface::linear_hashed_marching_cubes::LinearHashedMarchingCubes;
 use isosurface::source::CentralDifference;
 use isosurface::source::Source;
-use noise;
+use Noise;
 
-struct noise {}
+struct Noise {}
 
 const LOD: usize = 6;
 const DELTA: f32 = 0.005;
@@ -11,7 +11,7 @@ const SCALE: f32 = 2.1;
 
 impl Source for noise {
     fn sample(&self, x: f32, y: f32, z: f32) -> f32 {
-        let result = noise::de(x * SCALE, y * SCALE, z * SCALE);
+        let result = Noise::de(x * SCALE, y * SCALE, z * SCALE);
         if result.is_nan() {
             println!("NaN at: {} {} {}", x, y, z);
             0.0
@@ -25,7 +25,7 @@ pub fn gen_noise() -> (Vec<f32>, Vec<u32>) {
     let mut pos_normal = Vec::new();
     let mut index = Vec::new();
 
-    let source = CentralDifference::new(Box::new(noise {}));
+    let source = CentralDifference::new(Box::new(Noise {}));
     let mut linear_hashed_marching_cubes = LinearHashedMarchingCubes::new(LOD);
     linear_hashed_marching_cubes.extract_with_normals(&source, &mut pos_normal, &mut index);
 
