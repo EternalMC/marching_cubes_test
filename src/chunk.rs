080719ffc520f74966cba80874728cc3372e066d
EternalMC <55027815+EternalMC@users.noreply.github.com>
2020-03-12 11:56:05 -0500

    Update chunk.rs

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,25 +1,8 @@
 use isosurface::linear_hashed_marching_cubes::LinearHashedMarchingCubes;
 use isosurface::source::CentralDifference;
 use isosurface::source::Source;
-use mandelbox;
-
-struct Mandelbox {}
 
 const LOD: usize = 6;
-const DELTA: f32 = 0.005;
-const SCALE: f32 = 2.1;
-
-impl Source for Mandelbox {
-    fn sample(&self, x: f32, y: f32, z: f32) -> f32 {
-        let result = mandelbox::de(x * SCALE, y * SCALE, z * SCALE);
-        if result.is_nan() {
-            println!("NaN at: {} {} {}", x, y, z);
-            0.0
-        } else {
-            result - DELTA
-        }
-    }
-}
 
 pub fn gen_mbox() -> (Vec<f32>, Vec<u32>) {
     let mut pos_normal = Vec::new();
